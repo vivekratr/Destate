8202cf9ae2c711ecc37583c7ae90ff2858b1f438
Vivek Shah <95999628+vivekratr@users.noreply.github.com>
2024-06-10 18:00:03 +0000

    Handling unnecessary dependencies

--- server/src/utils.py
+++
@@ -1,6 +0,0 @@
-NO = "your key here"
-import os
-current_directory = os.getcwd()
-DATABASE = "D:/Abhi-theek-kar-k-deta-hu/artifacts/database.csv"
-LOCATION = "D:/Abhi-theek-kar-k-deta-hu/artifacts/location.csv"
-REPORT = "D:/Abhi-theek-kar-k-deta-hu/notebooks/report.docx"
